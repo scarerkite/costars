7346266a47167cef82040388468c8290f02453e8
Sarah Knight <scarerkite@users.noreply.github.com>
2025-06-02 17:13:22 +0100

    Add .gitignore, remove node_modules from tracking, add views folder

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const ejs = require('ejs');
 
@@ -6,7 +7,13 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render('index');
+});
+
+app.get('/search-actors', (req, res) => {
+  console.log('Actor 1:', req.query.actor1);
+  console.log('Actor 2:', req.query.actor2);
+  res.send('Search functionality coming soon!');
 });
 
 app.listen(3000, () => {
