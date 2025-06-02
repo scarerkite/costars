26d1e43b1b4c117b4db21f2b83ada613151c1130
Sarah Knight <scarerkite@users.noreply.github.com>
2025-06-02 15:26:40 +0100

    Get express and ejs set up

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const ejs = require('ejs');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
