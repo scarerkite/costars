2c58ef32abd15e71855f5ed1fd3439f23bab5f08
Sarah Knight <scarerkite@users.noreply.github.com>
2025-06-12 16:05:40 +0100

    Docker changes ready for deployment

--- Dockerfile
+++ Dockerfile
@@ -7,5 +7,6 @@ COPY *.js .
 COPY public/ ./public/
 COPY test/ ./test/
 COPY views/ ./views/
+EXPOSE 3000
 ENTRYPOINT ["node"]
 CMD ["server.js"]
--- server.js
+++ server.js
@@ -69,11 +69,10 @@ app.get('/search-actors', async (req, res) => {
 });
 
 if (process.env.NODE_ENV !== 'test') {
-  app.listen(3000, () => {
-    console.log('Server started on port 3000');
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server started on port ${PORT}`);
   });
 }
-
-console.log("Docker works!");
 
 export default app;
